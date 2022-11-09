a56aee9aa2170c3b4d93c8414efaa81d90eb9b2b
k-muell <62077276+k-muell@users.noreply.github.com>
2022-11-09 16:43:11 -0600

    Basis for CommandPrompt function

--- TechShell.c
+++ TechShell.c
@@ -1,3 +1,20 @@
-#CSC222 Project
-#Keith Mueller and Katie Sparr
-#14 NOV 2022
+
+//CSC22 Project
+//Keith Mueller and Katie Sparr
+//14 NOV 2022
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char* CommandPrompt()
+{
+        char cwd[500];
+        getcwd (cwd, sizeof(cwd));
+        printf("\n%s$", cwd);
+}
+
+int main()
+{       
+        CommandPrompt();
+}   
